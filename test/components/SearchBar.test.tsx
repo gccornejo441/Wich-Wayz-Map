1f4b41e080c588c68d1a5b5378784301da8fe6bf
Gabriel Cornejo <gcornejo441@gmail.com>
2025-01-03 09:40:31 -0500

    feat(test): creates a test case for test suggestions

--- test/components/SearchBar.test.tsx
+++ test/components/SearchBar.test.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import SearchBar from "../../src/components/Search/SearchBar";
+import { SearchShops } from "../../src/services/search";
 import { useMap } from "../../src/context/mapContext";
 import "@testing-library/jest-dom";
-import { vi } from "vitest"; // Ensure vi is imported
+import { vi } from "vitest";
+import userEvent from "@testing-library/user-event";
 
-// Mock the dependencies
 vi.mock("../../src/services/search", () => ({
   SearchShops: vi.fn(),
 }));
@@ -41,5 +42,47 @@ describe("SearchBar", () => {
     render(<SearchBar />);
     const searchIcon = screen.getByRole("img", { hidden: true });
     expect(searchIcon).toBeInTheDocument();
+  });
+
+  it("fetches and displays suggestions when typing", async () => {
+    const mockSuggestions = [
+      {
+        shop: {
+          id: 1,
+          name: "Molinari Delicatessen",
+          locations: [
+            { street_address: "373 Columbus Ave", city: "San Francisco" },
+          ],
+        },
+      },
+      {
+        shop: {
+          id: 2,
+          name: "Mr Mustache",
+          locations: [{ street_address: "Flower Street", city: "Pasadena" }],
+        },
+      },
+    ];
+
+    (SearchShops as jest.Mock).mockResolvedValue(mockSuggestions);
+
+    render(<SearchBar />);
+
+    const input = screen.getByPlaceholderText("Search shops");
+
+    userEvent.type(input, "Shop");
+
+    await waitFor(() => {
+      expect(SearchShops).toHaveBeenCalledWith("Shop");
+      expect(screen.getByRole("combobox")).toHaveAttribute(
+        "aria-expanded",
+        "true",
+      );
+    });
+
+    const suggestions = await screen.findAllByText(
+      /Molinari Delicatessen|Mr Mustache/,
+    );
+    expect(suggestions).toHaveLength(2);
   });
 });
