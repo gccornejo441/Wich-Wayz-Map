896abf017569f5fe023dedc8f3ee33b2bcdb1062
Gabriel Cornejo <gcornejo441@gmail.com>
2025-03-01 15:16:43 -0500

    feat: improve UI layout and styling for UserLeaderboard component

--- src/components/App/UserLeaderboard.tsx
+++ src/components/App/UserLeaderboard.tsx
@@ -23,7 +23,7 @@ const LeaderboardList = ({
             <span className="text-lg font-bold text-accent w-6">
               {index + 1}
             </span>
-            <div className="w-12 h-12 rounded-full border-2 border-primaryBorder bg-gray-200 flex items-center justify-center cursor-pointer">
+            <div className="w-12 h-12 md:rounded-full  md:border-2 md:border-primaryBorder md:bg-gray-200 flex items-center justify-center cursor-pointer">
               {user.avatar ? (
                 <UserAvatar
                   avatarId={user.avatar}
@@ -101,8 +101,8 @@ const UserLeaderboard = () => {
   return (
     <div className="min-h-screen bg-background p-6 mt-10 font-poppins">
       <header className="mb-8 text-center">
-        <h1 className="text-4xl font-extrabold text-dark">
-          Wich Wayz Leaderboards
+        <h1 className="text-3xl md:text-4xl font-extrabold text-dark">
+          Wich Wayz?<br/>Leaderboards
         </h1>
       </header>
 
@@ -110,7 +110,7 @@ const UserLeaderboard = () => {
         {stats.map((stat, index) => (
           <div
             key={index}
-            className={`p-6 rounded-xl text-secondary ${stat.color} shadow-card text-center`}
+            className={`p-4 md:p-6 rounded-xl text-secondary ${stat.color} shadow-card text-center`}
           >
             <h2 className="text-xl font-semibold">{stat.label}</h2>
             <p className="text-3xl font-bold mt-2">{stat.value}</p>
@@ -119,7 +119,7 @@ const UserLeaderboard = () => {
       </div>
 
       <div className="max-w-2xl mx-auto bg-lightGray shadow-card p-6 rounded-xl">
-        <h2 className="text-2xl font-bold mb-4 text-primary">
+        <h2 className="text-center md:text-left text-2xl font-bold mb-4 text-primary">
           Top Sandwich Contributors
         </h2>
         <LeaderboardList leaderboard={leaderboard} />
