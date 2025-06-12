e41b5b952e908e959976697b263ece34f719cab9
Gabriel Cornejo <gcornejo441@gmail.com>
2025-06-12 18:29:02 -0400

    fix: update version in package.json and adjust loading overlay dimensions in MapBox component

--- src/components/Map/MapBox.tsx
+++ src/components/Map/MapBox.tsx
@@ -221,7 +221,7 @@ const MapBox = () => {
   return (
     <div>
       {loading && (
-        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-surface-light dark:bg-surface-dark opacity-75 z-50">
+        <div className="absolute top-0 left-0 w-[100vw] h-[100vh] flex items-center justify-center bg-surface-light dark:bg-surface-dark opacity-75 z-50">
           <GiSandwich className="animate-spin text-[50px] text-brand-primary dark:text-brand-secondary mr-4" />
           <span className="text-brand-primary dark:text-brand-secondary text-md font-semibold">
             {loadingCaption}
